184055c26b90faca6914037492e192406e34ba5f
MossaJehad <Moosa.jehad65@gmail.com>
2025-08-23 18:30:36 +0300

    libft norm

--- libft/ft_itoa_buf.c
+++ libft/ft_itoa_buf.c
@@ -6,27 +6,27 @@
 /*   By: mhasoneh <mhasoneh@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 03:55:19 by mhasoneh          #+#    #+#             */
-/*   Updated: 2025/08/22 00:19:28 by mhasoneh         ###   ########.fr       */
+/*   Updated: 2025/08/23 18:26:06 by mhasoneh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_itoa_buf(int n, char *buf, int i, int j)
+static int	fill_digits(int n, char *tmp)
 {
-	char	tmp[12];
-	int		neg;
+	int	i;
+	int	neg;
 
+	i = 0;
 	neg = 0;
-	if (n == 0)
-	{
-		buf[0] = '0';
-		buf[1] = '\0';
-		return ;
-	}
 	if (n < 0)
 	{
 		neg = 1;
+		if (n == -2147483648)
+		{
+			tmp[i++] = '8';
+			n /= 10;
+		}
 		n = -n;
 	}
 	while (n > 0)
@@ -36,10 +36,25 @@ void	ft_itoa_buf(int n, char *buf, int i, int j)
 	}
 	if (neg)
 		tmp[i++] = '-';
-	while (j < i)
+	return (i);
+}
+
+void	ft_itoa_buf(int n, char *buf, int i, int j)
+{
+	char	tmp[12];
+	int		len;
+
+	if (n == 0)
 	{
-		buf[j] = tmp[i - j - 1];
+		buf[0] = '0';
+		buf[1] = '\0';
+		return ;
+	}
+	len = fill_digits(n, tmp);
+	while (j < len)
+	{
+		buf[j] = tmp[len - j - 1];
 		j++;
 	}
-	buf[i] = '\0';
+	buf[len] = '\0';
 }
--- libft/ft_strtok.c
+++ libft/ft_strtok.c
@@ -6,40 +6,17 @@
 /*   By: mhasoneh <mhasoneh@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 17:30:50 by mhasoneh          #+#    #+#             */
-/*   Updated: 2025/08/10 14:20:12 by mhasoneh         ###   ########.fr       */
+/*   Updated: 2025/08/23 18:30:02 by mhasoneh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/*
+		Under Construction
+*/
 char	*ft_strtok(char *str, const char *delim)
 {
-	char	*save;
-	char	*token;
-	int		i;
-
-	i = 0;
-	if (str)
-		save = str;
-	else
-		save = NULL;
-	if (!save || *save == '\0')
-		return (NULL);
-	while (*save && ft_strchr(delim, *save))
-		save++;
-	if (*save == '\0')
-		return (NULL);
-	token = save;
-	while (save[i] && !ft_strchr(delim, save[i]))
-		i++;
-	if (save[i])
-	{
-		save[i] = '\0';
-		save = &save[i + 1];
-	}
-	else
-		save = NULL;
-	return (token);
+	return (NULL);
 }
 
 char	*path_join(const char *dir, const char *file)
@@ -47,6 +24,5 @@ char	*path_join(const char *dir, const char *file)
 	char	*result;
 
 	result = malloc(ft_strlen(dir) + ft_strlen(file) + 2);
-	sprintf(result, "%s/%s", dir, file);
 	return (result);
 }
