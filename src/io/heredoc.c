499635a4534d9229af008676d457f30517ac3167
MossaJehad <Moosa.jehad65@gmail.com>
2025-08-27 19:30:47 +0300

    Now heredoc are only in parent

--- src/io/heredoc.c
+++ src/io/heredoc.c
@@ -30,48 +30,31 @@ int	handle_heredoc_child(int pipefd[2], t_token *tok)
 	{
 		close(pipefd[1]);
 		//close_heredoc_fds(ctx.heredoc_fds, ctx.cmd_count);
-		exit(1);
+		return (1);
 	}
 	close(pipefd[1]);
 	//close_heredoc_fds(ctx.heredoc_fds, ctx.cmd_count);
-	exit(0);
+	return (0);
 }
 
 int	handle_heredoc_parent(pid_t pid, int pipefd[2], int *status)
 {
 	close(pipefd[1]);
-	waitpid(pid, status, 0);
-	if (WIFSIGNALED(*status) && WTERMSIG(*status) == SIGINT)
-	{
-		close(pipefd[0]);
-		g_signal = 130;
-		return (-1);
-	}
+	(void)pid;
+	(void)status;
 	return (pipefd[0]);
 }
 
 int	handle_heredoc(t_token *tok, t_exec_ctx *ctx)
 {
 	int		pipefd[2];
-	pid_t	pid;
+	pid_t	pid = 0;
 	int		status;
 
 	(void)ctx;
 	if (create_heredoc_pipe(pipefd) == -1)
 		return (-1);
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		close(pipefd[0]);
-		close(pipefd[1]);
-		return (-1);
-	}
-	if (pid == 0)
-	{
-		//close_heredoc_fds(ctx->heredoc_fds, -1);
-		handle_heredoc_child(pipefd, tok);
-	}
+	handle_heredoc_child(pipefd, tok);
 	return (handle_heredoc_parent(pid, pipefd, &status));
 }
 
